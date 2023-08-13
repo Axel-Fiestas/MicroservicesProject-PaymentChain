4dbc0cacaa5b4c7583b66cbaff61caed8e533dc0
Axel Fiestas <josue123452002@gmail.com>
2023-08-12 19:16:56 -0500

    feat(CustomerTransaction): New attributes added with a transactionId

--- businessdomain/customer/src/main/java/com/paymentchain/customer/entities/CustomerTransaction.java
+++ businessdomain/customer/src/main/java/com/paymentchain/customer/entities/CustomerTransaction.java
@@ -1,8 +1,10 @@
 package com.paymentchain.customer.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
@@ -11,9 +13,18 @@ public class CustomerTransaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
-    private String accountIban;
+    private long transactionId;
     @Transient
-    private double balance;
+    private Date dateTime;
+    @Transient
+    private double amount;
+    @Transient
+    private String description;
+    @Transient
+    private String status;
+    @Transient
+    private String channel;
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY,targetEntity = Customer.class)
     @JoinColumn(name = "customerId",nullable = true)
     private Customer customer;
